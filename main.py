40d7eb08a3770fb17779db0db7627707851bb33b
lududuze <18639167774@163.com>
2022-10-22 22:24:57 +0800

    可以改格式了

--- main.py
+++ main.py
@@ -15,32 +15,33 @@ if __name__ == "__main__":
 		cellb = sheet.cell(row=j, column=6)
 		cellc = sheet.cell(row=j, column=7)
 		celld = sheet.cell(row=j, column=8)
+		i = 0
 		for paragraph in doc.paragraphs:
-			i=0
-			if '{题目}' in paragraph.text:
-				# 如果存在匹配得字符串，那么将当前得run替换成合并后得字符串
-				paragraph.text = paragraph.text.replace('{题目}', str(cell1.value))
-				i+=1
-				break
-			elif '{选项A}' in paragraph.text:
-				# 如果存在匹配得字符串，那么将当前得run替换成合并后得字符串
-				paragraph.text = paragraph.text.replace('{选项A}', str(cella.value))
-				i += 1
-				break
-			elif '{选项B}' in paragraph.text:
-				# 如果存在匹配得字符串，那么将当前得run替换成合并后得字符串
-				paragraph.text = paragraph.text.replace('{选项B}', str(cellb.value))
-				i += 1
-				break
-			elif '{选项C}' in paragraph.text:
-				# 如果存在匹配得字符串，那么将当前得run替换成合并后得字符串
-				paragraph.text = paragraph.text.replace('{选项C}', str(cellc.value))
-				i += 1
-				break
-			elif '{选项D}' in paragraph.text:
-				# 如果存在匹配得字符串，那么将当前得run替换成合并后得字符串
-				paragraph.text = paragraph.text.replace('{选项D}', str(celld.value))
-				i += 1
-				break
-			if i==5:break
+			for run in paragraph.runs:
+				if '{题目}' in run.text:
+					# 如果存在匹配得字符串，那么将当前得run替换成合并后得字符串
+					run.text = run.text.replace('{题目}', str(cell1.value))
+					i += 1
+
+				elif '{选项A}' in run.text:
+					# 如果存在匹配得字符串，那么将当前得run替换成合并后得字符串
+					run.text = run.text.replace('{选项A}', str(cella.value))
+					i += 1
+
+				elif '{选项B}' in run.text:
+					# 如果存在匹配得字符串，那么将当前得run替换成合并后得字符串
+					run.text = run.text.replace('{选项B}', str(cellb.value))
+					i += 1
+
+				elif '{选项C}' in run.text:
+					# 如果存在匹配得字符串，那么将当前得run替换成合并后得字符串
+					run.text = run.text.replace('{选项C}', str(cellc.value))
+					i += 1
+
+				elif '{选项D}' in run.text:
+					# 如果存在匹配得字符串，那么将当前得run替换成合并后得字符串
+					run.text = run.text.replace('{选项D}', str(celld.value))
+					i += 1
+
+				if i == 5: break
 	doc.save('shijuan.docx')
